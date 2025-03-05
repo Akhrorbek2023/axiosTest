e825f3c804b507f8bcc5f1349e21a477789b665d
Ahrorbek <199riddik@gmail.com>
2025-03-05 10:17:07 +0500

    Button rangi qizilga o'zgartirildi

--- src/components/MovieHeader.jsx
+++ src/components/MovieHeader.jsx
@@ -10,7 +10,7 @@ const MovieHeader = () => {
         <div className="container px-4 mx-auto">
             <div className='flex justify-between items-center'>
                 <h1 className='font-bold text-2xl'>My favourite Movies</h1>
-                <button onClick={() => setShowMovieModal(true)} className='bg-green-400 rounded-md py-3 px-6 text-white hover:scale-105'>Add Movie</button>
+                <button onClick={() => setShowMovieModal(true)} className='bg-red-400 rounded-md py-3 px-6 text-white hover:scale-105'>Add Movie</button>
             </div>
         </div>
     </div>
